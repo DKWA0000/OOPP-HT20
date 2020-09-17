e8d335dd0cdd796922d972f64411afab4c059433
FHanberg <filhanb@student.chalmers.se>
2020-09-17 13:10:34 +0200

    Started work on GUI

--- app/src/main/java/com/example/planka/MainActivity.java
+++ app/src/main/java/com/example/planka/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.planka;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView test = findViewById(R.id.test);
+        test.setText("Test");
     }
 }
