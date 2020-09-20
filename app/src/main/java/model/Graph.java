504d395f5a54b77831183384e7857ad43c0a1bfe
JaoK <jjaokk@gmail.com>
2020-09-20 20:29:37 +0200

    made Node an own class, not complete
    TODO: fix method getOuterType()

--- app/src/main/java/model/Graph.java
+++ app/src/main/java/model/Graph.java
@@ -1,3 +1,6 @@
+package model;
+
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -165,51 +168,5 @@ public class Graph {
         AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
         AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
      */
-    class Node {
-        String station;
-        Node(String station) {
-            this.station = station;
-        }
 
-        @Override
-        /*
-            To make it easier to find our nodes.
-        */
-        public int hashCode() {
-            final int prime = 31;
-            int result = 1;
-            result = prime * result + getOuterType().hashCode();
-            result = prime * result + ((station == null) ? 0 : station.hashCode());
-            return result;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj)
-                return true;
-            if (obj == null)
-                return false;
-            if (getClass() != obj.getClass())
-                return false;
-            Node other = (Node) obj;
-            if (!getOuterType().equals(other.getOuterType()))
-                return false;
-            if (station == null) {
-                if (other.station != null)
-                    return false;
-            } else if (!station.equals(other.station))
-                return false;
-            return true;
-        }
-
-        @Override
-        public String toString() {
-            return station;
-        }
-
-
-        private Graph getOuterType() {
-            return Graph.this;
-        }
-    }
 }
