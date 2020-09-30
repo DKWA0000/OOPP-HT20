a99633b845d7496314fca65bfddc8419398834ec
JaoK <jjaokk@gmail.com>
2020-09-30 10:22:05 +0200

    refactor

--- app/src/main/java/com/example/planka/IncidentItemView.java
+++ app/src/main/java/com/example/planka/IncidentItemView.java
@@ -6,26 +6,26 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
-public class IncidentViewItem extends ConstraintLayout {
+public class IncidentItemView extends ConstraintLayout {
     //Class which displays information regarding a given incident.
     TextView mPositionLabel;
 
-    public IncidentViewItem(Context context) {
+    public IncidentItemView(Context context) {
         super(context);
-        inflate(context,R.layout.incidentviewitem,this);
+        inflate(context,R.layout.incidentitemview,this);
         Init();
     }
 
-    public IncidentViewItem(Context context, AttributeSet attrs) {
+    public IncidentItemView(Context context, AttributeSet attrs) {
         //this is the constructor that currently gets called
         super(context, attrs);
-        inflate(context,R.layout.incidentviewitem,this);
+        inflate(context,R.layout.incidentitemview,this);
         Init();
     }
 
-    public IncidentViewItem(Context context, AttributeSet attrs, int defStyle) {
+    public IncidentItemView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        inflate(context,R.layout.incidentviewitem,this);
+        inflate(context,R.layout.incidentitemview,this);
         Init();
     }
 
