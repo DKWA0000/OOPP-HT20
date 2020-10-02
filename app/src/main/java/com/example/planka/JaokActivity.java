3bf473cd96e7e87952534bec7f6ce96ce679d94d
JaoK <jjaokk@gmail.com>
2020-10-02 14:58:08 +0200

    refactor methods

--- app/src/main/java/com/example/planka/JaokActivity.java
+++ app/src/main/java/com/example/planka/JaokActivity.java
@@ -17,9 +17,54 @@ public class JaokActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.report_activity);
-        toLocation();
-        spinnerListeners();
+
         model = new MODEL(getAssets());
+
+        // main tab
+        toLocation();
+
+        initSpinners();
+        spinnerListeners();
+
+        setLineDropdown(View.INVISIBLE);
+        setWhenDropdown(View.GONE);
+
+
+        initAutoFill();
+
+        loadReports();
+        loadIncidents();
+
+        model.addObserver(() -> {
+
+            AbstractReport report = model.getLatestReport();
+
+            UserReportView urw = new UserReportView(getBaseContext(),report.getStation().getName(),report.getTimeOfReport().toString(),report.getnControllants());
+            ((LinearLayout)findViewById(R.id.Reportlist)).addView(urw);
+
+            Incident i = model.getLatestIncident();
+
+            IncidentView iw = new IncidentView(getBaseContext(), i);
+            ((LinearLayout)findViewById(R.id.Incidentlist)).addView(iw);
+
+
+        });
+
+    }
+
+    private void loadIncidents() {
+        //TODO:
+    }
+
+    private void initAutoFill() {
+        String[] list = model.getAllStations();
+
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, list);
+
+        ((AutoCompleteTextView)findViewById(R.id.stationTextBox)).setAdapter(adapter2);
+    }
+
+    private void initSpinners() {
 
         Spinner spinner = (Spinner) findViewById(R.id.lineSpinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
@@ -44,34 +89,6 @@ public class JaokActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
-
-
-        hideLineDropdown(findViewById(0));
-        hideWhenDropdown(findViewById(0));
-
-        String[] list = model.getAllStations();
-
-        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, list);
-
-        ((AutoCompleteTextView)findViewById(R.id.stationTextBox)).setAdapter(adapter2);
-
-
-        loadReports();
-
-        model.addObserver(() -> {
-
-               AbstractReport report = model.getLatestReport();
-
-            UserReportView urw = new UserReportView(getBaseContext(),report.getStation().getName(),report.getTimeOfReport().toString(),report.getnControllants());
-            ((LinearLayout)findViewById(R.id.Reportlist)).addView(urw);
-
-            Incident i = model.getLatestIncident();
-
-            IncidentView iw = new IncidentView(getBaseContext(), i);
-            ((LinearLayout)findViewById(R.id.Incidentlist)).addView(iw);
-
-
-        });
 
     }
 
@@ -108,48 +125,64 @@ public class JaokActivity extends AppCompatActivity {
         if (view == findViewById(R.id.myReportsText)) {
             toMyReports();
         }
+        if(view == findViewById(R.id.stationRadio)){
+            setStationDropDown(View.VISIBLE);
+        }
+        if(view == findViewById(R.id.tramRadio)){
+            setStationDropDown(View.INVISIBLE);
+        }
+        if(view == findViewById(R.id.nowRadio)){
+            setWhenDropdown(View.GONE);
+        }
+        if(view == findViewById(R.id.pastRadio)){
+            setWhenDropdown(View.VISIBLE);
+        }
+        if(view == findViewById(R.id.makeReportButton)){
+            makeReport();
+        }
+
 
     }
 
 
-    public void activateLocationButton(){
+    private void activateLocationButton(){
         findViewById(R.id.locationsButton).setForeground(getDrawable(R.drawable.location_icon_active));
         findViewById(R.id.mainLocationView).setVisibility(View.VISIBLE);
     }
 
-    public void inactivateLocationButton(){
+    private void inactivateLocationButton(){
         findViewById(R.id.locationsButton).setForeground(getDrawable(R.drawable.location_icon));
         findViewById(R.id.mainLocationView).setVisibility(View.INVISIBLE);
     }
 
-    public void activateReportButton(){
+    private void activateReportButton(){
         findViewById(R.id.reportsButton).setForeground(getDrawable(R.drawable.report_icon_active));
         findViewById(R.id.mainReportView).setVisibility(View.VISIBLE);
     }
 
-    public void inactivateReportButton(){
+    private void inactivateReportButton(){
         findViewById(R.id.reportsButton).setForeground(getDrawable(R.drawable.report_icon));
         findViewById(R.id.mainReportView).setVisibility(View.INVISIBLE);
     }
 
-    public void activateProfileButton(){
+    private void activateProfileButton(){
         findViewById(R.id.profileButton).setForeground(getDrawable(R.drawable.profile_icon_active));
         findViewById(R.id.mainProfileView).setVisibility(View.VISIBLE);
     }
 
-    public void inactivateProfileButton(){
+    private void inactivateProfileButton(){
         findViewById(R.id.profileButton).setForeground(getDrawable(R.drawable.profile_icon));
         findViewById(R.id.mainProfileView).setVisibility(View.INVISIBLE);
     }
 
 
-    public void toLocation() {
+    private void toLocation() {
         activateLocationButton();
         inactivateReportButton();
         inactivateProfileButton();
     }
 
-    public void toReport() {
+    private void toReport() {
         inactivateLocationButton();
         activateReportButton();
         inactivateProfileButton();
@@ -157,13 +190,13 @@ public class JaokActivity extends AppCompatActivity {
         toMakeReport();
     }
 
-    public void toProfile() {
+    private void toProfile() {
         inactivateLocationButton();
         inactivateReportButton();
         activateProfileButton();
     }
 
-    public void toMyReports() {
+    private void toMyReports() {
         ((TextView)findViewById(R.id.myReportsText)).setTextColor(getColor(R.color.smurf));
         ((TextView)findViewById(R.id.makeReportText)).setTextColor(getColor(R.color.text_gray));
 
@@ -171,55 +204,37 @@ public class JaokActivity extends AppCompatActivity {
         findViewById(R.id.myReportsView).setVisibility(View.VISIBLE);
     }
 
-    public void toMakeReport() {
+    private void toMakeReport() {
         ((TextView)findViewById(R.id.myReportsText)).setTextColor(getColor(R.color.text_gray));
         ((TextView)findViewById(R.id.makeReportText)).setTextColor(getColor(R.color.smurf));
-
 
         findViewById(R.id.reportFormView).setVisibility(View.VISIBLE);
         findViewById(R.id.myReportsView).setVisibility(View.INVISIBLE);
     }
 
 
-    public void showStationDropdown(View view) {
-        hideLineDropdown(view);
-        findViewById(R.id.stationTextBox).setVisibility(View.VISIBLE);
-        findViewById(R.id.stationText).setVisibility(View.VISIBLE);
-        findViewById(R.id.lineDivider).setVisibility(View.VISIBLE);
+    private void setStationDropDown(int visible) {
+
+        if(visible == View.VISIBLE)
+           setLineDropdown(View.INVISIBLE);
+        else
+            setLineDropdown(View.VISIBLE);
+
+        findViewById(R.id.stationTextBox).setVisibility(visible);
+        findViewById(R.id.stationText).setVisibility(visible);
+        findViewById(R.id.lineDivider).setVisibility(visible);
     }
 
-    public void hideStationDropdown(View view) {
-        findViewById(R.id.stationTextBox).setVisibility(View.INVISIBLE);
-        findViewById(R.id.stationText).setVisibility(View.INVISIBLE);
-        findViewById(R.id.lineDivider).setVisibility(View.INVISIBLE);
+    private void setLineDropdown(int visible) {
+        findViewById(R.id.lineDropdownText).setVisibility(visible);
+        findViewById(R.id.lineDivider).setVisibility(visible);
+        findViewById(R.id.lineSpinner).setVisibility(visible);
     }
 
-
-    public void showLineDropdown(View view) {
-        hideStationDropdown(view);
-        findViewById(R.id.lineDropdownText).setVisibility(View.VISIBLE);
-        findViewById(R.id.lineDivider).setVisibility(View.VISIBLE);
-        findViewById(R.id.lineSpinner).setVisibility(View.VISIBLE);
-    }
-
-
-
-    public void hideLineDropdown(View view) {
-        findViewById(R.id.lineDropdownText).setVisibility(View.INVISIBLE);
-        findViewById(R.id.lineDivider).setVisibility(View.INVISIBLE);
-        findViewById(R.id.lineSpinner).setVisibility(View.INVISIBLE);
-    }
-
-    public void showWhenDropdown(View view) {
-        findViewById(R.id.whenDropdownText).setVisibility(View.VISIBLE);
-        findViewById(R.id.whenDivider).setVisibility(View.VISIBLE);
-        findViewById(R.id.whenSpinner).setVisibility(View.VISIBLE);
-    }
-
-    public void hideWhenDropdown(View view) {
-        findViewById(R.id.whenDropdownText).setVisibility(View.GONE);
-        findViewById(R.id.whenDivider).setVisibility(View.GONE);
-        findViewById(R.id.whenSpinner).setVisibility(View.GONE);
+    private void setWhenDropdown(int visible) {
+        findViewById(R.id.whenDropdownText).setVisibility(visible);
+        findViewById(R.id.whenDivider).setVisibility(visible);
+        findViewById(R.id.whenSpinner).setVisibility(visible);
     }
 
     String noContr;
@@ -227,7 +242,7 @@ public class JaokActivity extends AppCompatActivity {
     String image;
     String station;
 
-    public void spinnerListeners(){
+    private void spinnerListeners(){
         ((Spinner)findViewById(R.id.controllantsSpinner)).setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
@@ -269,12 +284,9 @@ public class JaokActivity extends AppCompatActivity {
     }
 
 
-    public void makeReport(View view){
-
+    private void makeReport(){
         String image = null;
-
         model.makeStationReport(noContr,time,image,station);
-
     }
 
 
