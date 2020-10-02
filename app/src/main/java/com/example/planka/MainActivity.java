7588fa5968a9fa7d9e92c5ee5b303b1c29f5073d
JaoK <jjaokk@gmail.com>
2020-10-02 16:15:23 +0200

    rename folders and main activity

--- app/src/main/java/com/example/planka/MainActivity.java
+++ app/src/main/java/com/example/planka/MainActivity.java
@@ -13,7 +13,7 @@ import java.time.Instant;
 import java.util.Date;
 
 
-public class JaokActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     private MODEL model;
 
