15e52931e22b069e6cb1e2e6171439a8458d5976
FHanberg <57720765+FHanberg@users.noreply.github.com>
2020-10-07 14:12:10 +0200

    Added features to edit reports; not fully functional

--- app/src/main/java/com/example/planka/EditReportView.java
+++ app/src/main/java/com/example/planka/EditReportView.java
@@ -5,20 +5,20 @@ import android.util.AttributeSet;
 import android.widget.TextView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-public class ReportFormView extends ConstraintLayout {
+public class EditReportView extends ConstraintLayout {
 
-    public ReportFormView(Context context) {
+    public EditReportView(Context context) {
         super(context);
         inflate(context,R.layout.reportformview,this);
     }
 
-    public ReportFormView(Context context, AttributeSet attrs) {
+    public EditReportView(Context context, AttributeSet attrs) {
         //this is the constructor that currently gets called
         super(context, attrs);
         inflate(context,R.layout.reportformview,this);
     }
 
-    public ReportFormView(Context context, AttributeSet attrs, int defStyle) {
+    public EditReportView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         inflate(context,R.layout.reportformview,this);
     }
